ebe9e174473f61e647d1b9c953cea47f1e38120a
esteban5tael <esteban5tael@gmail.com>
2025-01-16 07:28:51 -0500

    Remove compiled JavaScript files from the distribution directory

--- dist/app.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("dotenv/config");
-const server_1 = require("./server");
-const PORT = parseInt(process.env.PORT) || 4000;
-const GLOBAL_PREFIX = process.env.GLOBAL_PREFIX || "/api/v1";
-server_1.app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}${GLOBAL_PREFIX}`);
-});
